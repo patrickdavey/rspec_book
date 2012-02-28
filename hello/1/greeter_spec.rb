40deb4840f764d5f5a0f30a1d14258f1d4d0bf93
Patrick Davey <Patrick@egressive.com>
2012-02-28 15:37:50 +1300

    starting 2.3 Hello Cucumber

--- hello/1/greeter_spec.rb
+++ hello/1/greeter_spec.rb
@@ -1,3 +1,10 @@
+class RSpecGreeter
+  def greet
+    "Hello RSpec!"
+  end
+end
+
+
 describe "RSpec Greeter" do
   it "should say 'Hello RSpec!' when it receives the greet() message" do
     greeter = RSpecGreeter.new
