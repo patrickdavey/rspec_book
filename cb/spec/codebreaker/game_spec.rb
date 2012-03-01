a41b5f243beb196812f78265b727703012aa54a4
Patrick Davey <Patrick@egressive.com>
2012-03-01 13:28:23 +1300

    before working out how require is working

---
+++ cb/spec/codebreaker/game_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+module Codebreaker
+  describe Game do
+    describe "#start" do
+      it "sends a welcome mesasge"
+      it "prompts for the first guess"
+    end
+  end
+end
